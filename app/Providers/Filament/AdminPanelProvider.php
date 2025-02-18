7fa57a7addb8946e82954419906aafda2f3c55d6
0xyzone <58982560+0xyzone@users.noreply.github.com>
2025-02-19 02:27:54 +0545

    updates

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -17,7 +17,6 @@ use Jeffgreco13\FilamentBreezy\BreezyCore;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Filament\Http\Middleware\AuthenticateSession;
-use App\Filament\Players\Widgets\VerificationWidget;
 use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Hydrat\TableLayoutToggle\TableLayoutTogglePlugin;
 use Illuminate\Routing\Middleware\SubstituteBindings;
@@ -54,7 +53,6 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                VerificationWidget::class
             ])
             ->middleware([
                 EncryptCookies::class,
--- resources/views/filament/players/widgets/verification-widget.blade.php
+++ resources/views/filament/players/widgets/verification-widget.blade.php
@@ -1,14 +1,16 @@
 <x-filament-widgets::widget>
     <x-filament::section>
         <div class="flex md:flex-row flex-col gap-4 justify-between">
-            @if (!$verificationStatus)
+            @if (!$verificationStatus && $user->is_verified !== true)
             <div class="md:w-1/2">If you have completed your profile then you can</div>
             <form wire:submit.prevent="applyForVerification">
                 <button type="submit" class="px-6 py-2 bg-primary-500 text-white font-semibold rounded-lg hover:bg-primary-600 focus:outline-none focus:ring-2 focus:ring-primary-400 transition duration-200 flex-1 flex-shrink-0">
                     Apply for Verification
                 </button>
             </form>
-            @elseif ($verificationStatus->status === 'approved')
+            @elseif ($user->is_verified === true)
+            <p class="text-green-500 py-2">Your account has been verified!</p>
+            @elseif ($verificationStatus->status === 'approved' || $user->is_verified === true)
             <p class="text-green-500 py-2">Your account has been verified!</p>
             @elseif($verificationStatus->status === 'needs_revision')
             <div class="md:w-1/2 flex flex-col">
